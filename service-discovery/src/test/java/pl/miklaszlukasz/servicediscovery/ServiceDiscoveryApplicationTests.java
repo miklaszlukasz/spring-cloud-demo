38a098dacd6f2946fe56890d15c619cb530423fd
Łukasz Miklasz <miklaszlukasz@gmail.com>
2021-01-19 14:17:33 +0100

    init project

---
+++ service-discovery/src/test/java/pl/miklaszlukasz/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miklaszlukasz.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
